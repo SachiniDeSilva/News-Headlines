da665025e9cfa29ae47c602e237d60c9eb6c89bc
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-08-21 23:20:36 +0530

    page route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HeadlineList from './components/HeadlineList';
+import HeadlineDetail from './components/HeadlineDetail';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HeadlineList />} />
+        <Route path="/headline/:id" element={<HeadlineDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
